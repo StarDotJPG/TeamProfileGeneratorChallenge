6614dd70bbce2b60889db808c06bafb8ec1e98f1
Jason Greenia <jason.greenia@gmail.com>
2022-03-14 13:52:55 -0700

    created unit tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,11 @@
+const Manager = require('../lib/Manager.js');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Lenovo Lenny', '1237', 'lenovolenny@test.com', '456');
+
+    expect(manager.getName()).toBe('Lenovo Lenny');
+    expect(manager.getId()).toBe('1237');
+    expect(manager.getEmail()).toBe('lenovolenny@test.com');
+    expect(manager.getRole()).toBe('Manager');
+    expect(manager.getOfficeNumber()).toBe('456');
+})
