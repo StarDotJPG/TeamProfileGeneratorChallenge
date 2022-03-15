d39fc9b1264e69b4bdb4fb532f9225aeded6cd7c
Jason Greenia <jason.greenia@gmail.com>
2022-03-14 21:46:44 -0700

    refactored to ask team manager question first, then ask about the rest of the team members

--- index.js
+++ index.js
@@ -5,109 +5,141 @@ const Engineer = require('./lib/Engineer')
 const Manager = require('./lib/Manager')
 const Intern = require('./lib/Intern')
 const generatePage = require('./src/page-template')
-const { writeFile, copyFile } = require('./src/generate-site.js');
+const { writeFile } = require('./src/generate-site.js');
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the team manager\'s name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Manager name is required!')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is the manager\'s office number?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Office number is required!')
+                return false
+            }
+        }
+    }
+]
+
+const commonQuestions = [
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the employee?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Employee ID is required!')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the employee?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Employee email is required!')
+                return false
+            }
+        }
+    }
+]
+
+const confirmQuestion = [
+    {
+        type: 'confirm',
+        name: 'confirmAddMoreEmployees',
+        message: 'Would you like to add another employee?',
+        default: false
+    }
+]
+
+const nonManagerQuestions = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Add an employee to the team. What is the employee\'s role?',
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employee\'s name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Employee name is required!')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is the intern\'s school?',
+        when(answers) {
+            return answers.role == 'Intern';
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the engineer\'s github?',
+        when(answers) {
+            return answers.role == 'Engineer';
+        }
+    }
+]
 
 const promptEmployee = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the employee\'s name?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true
-                } else {
-                    console.log('Employee name is required!')
-                    return false
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the ID of the employee?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true
-                } else {
-                    console.log('Employee ID is required!')
-                    return false
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the email of the employee?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true
-                } else {
-                    console.log('Employee email is required!')
-                    return false
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'role',
-            message: 'What is the employee\'s role?',
-            choices: ['Engineer', 'Intern', 'Manager']
-        },
-        {
-            type: 'input',
-            name: 'officeNum',
-            message: 'What is the manager\'s office number?',
-            when(answers) {
-                return answers.role == 'Manager';
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What is the intern\'s school?',
-            when(answers) {
-                return answers.role == 'Intern';
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is the engineer\'s github?',
-            when(answers) {
-                return answers.role == 'Engineer';
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddMoreEmployees',
-            message: 'Would you like to add another employee?',
-            default: false
-        }
-    ]).then(employeeData => {
-        // If there's no 'employees' array, create one
-        if (typeof employees === 'undefined') {
-            employees = [];
-        }
-
-        // add the employee data to the employees objects array
-        if (employeeData.role == 'Engineer') {
-            employees.push(new Engineer(employeeData.name, employeeData.id, employeeData.email, employeeData.github))
-        }
-        if (employeeData.role == 'Manager') {
+    //If there's no 'employees' array, create one and ask for the manager (Like a Karen)
+    if (typeof employees === 'undefined') {
+        employees = [];
+        return inquirer.prompt([...managerQuestions, ...commonQuestions])
+        .then(employeeData => {
             employees.push(new Manager(employeeData.name, employeeData.id, employeeData.email, employeeData.officeNum))
-        }
-        if (employeeData.role == 'Intern') {
-            employees.push(new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school))
-        }
-
-        // if we need to add more employees, go to the start of the function, otherwise return
-        if (employeeData.confirmAddMoreEmployees) {
-            return promptEmployee();
-        } else {
-            return employees
-        }
-    })
+            return promptEmployee()
+        })
+    }
+    // if the employees array already exists, don't ask for the manager
+    return inquirer.prompt([...nonManagerQuestions, ...commonQuestions, confirmQuestion])
+        .then(employeeData => {
+            // add the employee data to the employees objects array
+            if (employeeData.role == 'Engineer') {
+                employees.push(new Engineer(employeeData.name, employeeData.id, employeeData.email, employeeData.github))
+            }
+            if (employeeData.role == 'Intern') {
+                employees.push(new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school))
+            }
+            
+            // if we need to add more employees, go to the start of the function, otherwise return
+            if (employeeData.confirmAddMoreEmployees) {
+                return promptEmployee();
+            } else {
+                return employees
+            }
+        })
 }
 
 // Function to initialize app
